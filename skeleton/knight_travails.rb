12a87df7ab77c0f397314f02f1e247587a6de9fb
ianellison87 <ianellison87@yahoo.com>
2019-05-17 18:38:39 -0700

    Add KnightPathFinder class

---
+++ skeleton/knight_travails.rb
@@ -0,0 +1,15 @@
+require_relative "00_tree_node"
+
+class KnightPathFinder
+  
+  def initialize(pos)
+    @pos = pos
+    @root_node = PolyTreeNode.new(pos)
+
+  end
+
+  def build_move_tree
+    
+  end
+
+end
